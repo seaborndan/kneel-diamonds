ab86741a3c5804926afd294e395eff4fb72bbc6a
Seaborn Mercer <surfermercer@gmail.com>
2022-10-07 09:26:43 -0500

    managing/setting state with data relationships and event listener practice

---
+++ scripts/Orders.js
@@ -0,0 +1,52 @@
+import { getOrders } from "./database.js"
+import { getMetals, getSizes, getStyles } from "./database.js"
+
+const metals = getMetals();
+const sizes = getSizes();
+const styles = getStyles();
+
+
+
+const buildOrderListItem = (order) => {
+    const foundMetal = metals.find(
+        (metal) => {
+            return metal.id === order.metalId
+        }
+    )
+    const foundSize = sizes.find(
+        (size) => {
+            return size.id === order.sizeId
+        }
+    )
+    const foundStyle = styles.find(
+        (style) => {
+            return style.id === order.styleId
+        }
+    )
+    const totalCost = foundMetal.price + foundSize.price + foundStyle.price;
+    const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+})
+    return `<li>
+        Order #${order.id} cost ${costString}
+    </li>`
+}
+
+export const Orders = () => {
+    /*
+        Can you explain why the state variable has to be inside
+        the component function for Orders, but not the others?
+    */
+    const orders = getOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
